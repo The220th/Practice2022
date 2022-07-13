394358cbefbe9ae5f6a92bea497082f8928c7648
The220th <you@example.com>
2022-07-13 22:18:52 +0300

    error detected begin

--- part2/subgraphk.py
+++ part2/subgraphk.py
@@ -19,6 +19,7 @@ def getOutStyle() -> dict:
     'pre': {
         'border': 'thin lightgrey solid',
         'overflowX': 'scroll'
+        #'overflowX': 'visible'
     }
 }
 
@@ -48,11 +49,20 @@ def getStylesheet() -> list:
                     'curve-style': 'bezier',
                     'target-arrow-shape': 'triangle',
                 }
+            },
+            {
+                'selector': '.triangle',
+                'style': {
+                    'shape': 'triangle',
+                    'background-color': 'red'
+                }
             }
     ]
     return style
 
 def getElements() -> list:
+    SYMBOLS_NUM = 10 # Количество отображаемых символов
+
     folderpath = Global.folderpath 
     files_buff = [os.path.join(path, name) for path, subdirs, files in os.walk(folderpath) for name in files]
     all_sub = {}
@@ -82,7 +92,45 @@ def getElements() -> list:
     all_k = all_sub.keys()
     all_vertex = []
     for k in all_k:
-        all_vertex.append({"data" : {"id" : f"{k}", "label" : f"{all_sub[k][:7]}", "sub_name" : f"{all_sub[k]}"}})
+        all_vertex.append({"data" : {"id" : f"{k}", "label" : f"{all_sub[k][:SYMBOLS_NUM]}", "sub_name" : f"{all_sub[k]}"}})
+
+    # Отобразить ошибки
+    # Если предмет a говорит, что дальше можно обучаться предмету b (classes == after and target == b), 
+    # а предмет b готорит, что ему не нужен предмет a (classes == before and not (source == a and target == b))
+    for k_int in all_k:
+        k = f"{k_int}"
+        for e_i in all_edges:
+            if(e_i["classes"] == "after" and e_i["data"]["target"] == k):
+                error_flag = True
+                k_source = e_i["data"]["source"]
+                for v_i in all_vertex:
+                    if(v_i["data"]["id"] == k_source):
+                        name_source = v_i["data"]["sub_name"]
+                        break
+
+                # Найти в before, что target == k, тогда ошибки нет
+                for e2_i in all_edges:
+                    if(e2_i["classes"] == "before" and e2_i["data"]["source"] == k_source and e2_i["data"]["target"] == k):
+                        error_flag = False
+                        break
+                
+                if(error_flag == True):
+                    #print(e_i)
+                    for v_i in all_vertex:
+                        if(v_i["data"]["id"] == k):
+                            subID = v_i["data"]["id"]
+                            subNAME = v_i["data"]["sub_name"]
+                            sub1 = f"\"{name_source}\" (id={k_source})"
+                            sub2 = f"\"{subNAME}\" (id={subID})"
+                            erMsg = f"* Предмет {sub1} говорит, что дальше нужно изучать {sub2}, но {sub2} не требует изучение {sub1}\n\n"
+                            if "error_msg" not in v_i["data"]:
+                                v_i["data"]["error_msg"] = erMsg
+                                v_i["classes"] = "triangle"
+                            else:
+                                v_i["data"]["error_msg"] += erMsg
+                
+
+    
     return all_vertex + all_edges
 
 @app.callback(Output('cytoscape-tapNodeData-json', 'children'),
@@ -91,7 +139,10 @@ def displayTapNodeData(data):
     #return json.dumps(data)
     if(data == None or data["sub_name"] == None):
         return "None"
-    return f"{data['sub_name']} (if={data['id']})"
+    res = f"{data['sub_name']} (if={data['id']})\n"
+    if("error_msg" in data):
+        res += f"\n{data['error_msg']}"
+    return res
 
 if __name__ == '__main__':
     buffPath = sys.argv[1]
@@ -106,7 +157,8 @@ if __name__ == '__main__':
                 id = "practice_part2",
                 #layout = {"name" : "random"},
                 #layout = {"name" : "circle"},
-                layout = {"name" : "breadthfirst", "roots" : [2026890]},
+                layout = {"name" : "breadthfirst"},
+                #layout = {"name" : "breadthfirst", "roots" : [2026890]},
                 style = {"width" : "100%", "height" : "720px"},
                 elements = getElements(),
                 stylesheet = getStylesheet()
